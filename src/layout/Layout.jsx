8be8e6912ae61b8751d0d7250b72dd357c336050
Lavkush Varma <lavkush3844@gmail.com>
2025-11-30 14:01:29 +0530

    user select none

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 export default function Layout({ children, title, showBackButton }) {
   return (
-    <div className="bg-gray-50 min-h-screen">
+    <div className="bg-gray-50 min-h-screen select-none">
       <Header title={title} showBackButton={showBackButton} />
 
       <main className="max-w-4xl mx-auto px-4 sm:px-6 py-6 min-h-[calc(100vh-80px)]">
